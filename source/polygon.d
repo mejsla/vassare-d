f648a02fc99336983f32bc0fe06a61263074cc52
dykstrom <jody4711-github@yahoo.se>
2020-10-11 16:25:27 +0200

    Added const/immutable example.

--- source/polygon.d
+++ source/polygon.d
@@ -4,21 +4,32 @@ import std.algorithm;
 import std.stdio;
 
 struct Point
-{ 
+{
     int x;
     int y;
-} 
+}
 
 struct Polygon(int n)
 {
     Point[n] corners;
 }
 
+alias Triangle = Polygon!3;
+
+void foo(Triangle triangle)
+{
+    foreach (point; triangle.corners)
+    {
+        writeln(point);
+        point.x = 0;
+    }
+}
+
 unittest
 {
-    alias Triangle = Polygon!3;
-    alias Rectangle = Polygon!4;
-
     assert(Triangle().corners.length == 3);
-    assert(Rectangle().corners.length == 4);
+
+    auto triangle = Triangle([Point(1, 2), Point(3, 4), Point(5, 6)]);
+    foo(triangle);
+    writeln(triangle);
 }
